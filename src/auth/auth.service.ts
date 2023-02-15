12221ac4a165ee2e600a8f8a285c170bb16b4c5d
Junior <eecfredes@gmail.com>
2023-02-15 20:47:56 -0300

    :rocket:feat: user validation service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
@@ -16,5 +17,24 @@ export class AuthService {
     const hash = await bcrypt.hash(user.password, salt);
     user.password = hash;
     return await this.userRepository.save(user);
+  }
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const foundUser = await this.userRepository.findOneBy({ username });
+    if (foundUser) {
+      if (await bcrypt.compare(password, foundUser.password)) {
+        const { password, ...result } = foundUser;
+        return result;
+      }
+      return null;
+    }
+    return null;
+  }
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id, role: user.role };
+
+    return {
+      access_token: this.jwt.sign(payload),
+    };
   }
 }
