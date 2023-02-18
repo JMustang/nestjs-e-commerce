f2c6eb63e04a4e018257cfec201542face539fba
Junior <eecfredes@gmail.com>
2023-02-17 21:55:23 -0300

    :rocket:feat: jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt-stategy';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './user.entity';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
