dca4a9e873e5d0c108ebe0e0ba385c567519f501
Junior <eecfredes@gmail.com>
2023-02-20 20:19:13 -0300

    :rocket:feat: Cart controller

---
+++ src/cart/cart.controller.ts
@@ -0,0 +1,33 @@
+import { JwtAuthGuard } from './../auth/jwt-guard';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { CartEntity } from './cart.entity';
+import { CartService } from './cart.service';
+
+@Controller('api/v1/cart')
+export class CartController {
+  constructor(private cartService: CartService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async AddToCart(@Body() body, @Request() req): Promise<void> {
+    const { productId, quantity } = body;
+    return await this.cartService.addToCart(
+      productId,
+      quantity,
+      req.user.username,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getItemsInCart(@Request() req): Promise<CartEntity[]> {
+    return await this.cartService.getItemsInCart(req.user.username);
+  }
+}
