67cec1193749554ecd3d8eabc7bd7ee2b87e1516
Junior <eecfredes@gmail.com>
2023-02-20 20:00:58 -0300

    :rocket:feat: Product controller

---
+++ src/product/products.controller.ts
@@ -0,0 +1,57 @@
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { ProductEntity } from './product.entity';
+import { JwtAuthGuard } from '../auth/jwt-guard';
+import { ProductsService } from './products.service';
+import {
+  Controller,
+  Delete,
+  Param,
+  Request,
+  UseGuards,
+  Get,
+  Post,
+  Put,
+  Body,
+} from '@nestjs/common';
+
+@Controller('api/v1/products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async GetAll(): Promise<ProductEntity[]> {
+    return await this.productsService.getAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async Create(
+    @Request() req,
+    @Body() product: ProductEntity,
+  ): Promise<ProductEntity> {
+    return await this.productsService.create(product, req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async GetOne(@Param() id: number): Promise<ProductEntity> {
+    return await this.productsService.getOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async Update(
+    @Param() id: number,
+    @Body() product: ProductEntity,
+    @Request() req,
+  ): Promise<UpdateResult> {
+    return await this.productsService.update(id, product, req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async Delete(@Param() id: number, @Request() req): Promise<DeleteResult> {
+    return await this.productsService.delete(id, req.user);
+  }
+}
